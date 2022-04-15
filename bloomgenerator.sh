7b4aa6d0f46e031d4d3de8ca4a18194e6cd5fca5
Alexey Deryugin <velovec@gmail.com>
2022-04-15 12:17:21 +0400

    You need some sleep

--- bloomgenerator.sh
+++ bloomgenerator.sh
@@ -4,8 +4,10 @@ set -xe
 
 FILE_NAME="/bloom_data/addrs_${1}.txt"
 FILE_SIZE=$(wc -l ${FILE_NAME})
-/src/bloomgenerator/bloomgenerator "${FILE_SIZE}" "${FILE_NAME}" "/bloom_data/bloom_data.${1}"
+/src/bloomgenerator/bloomgenerator ${FILE_SIZE} "/bloom_data/bloom_data.${1}"
 
 MD5=$(md5sum "/bloom_data/bloom_data.${1}")
 
 echo "${MD5} ${FILE_SIZE}" > "/bloom_data/bloom_data.${1}.meta"
+
+sleep 2
