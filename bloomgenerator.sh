d0056d0c64f9a6611537246dbe1561a4a314616b
Alexey Deryugin <velovec@gmail.com>
2022-04-15 12:11:48 +0400

    Add MD5 for bloom filters

--- bloomgenerator.sh
+++ bloomgenerator.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+set -xe
+
 FILE_NAME="/bloom_data/addrs_${1}.txt"
 FILE_SIZE=$(wc -l ${FILE_NAME})
 /src/bloomgenerator/bloomgenerator "${FILE_SIZE}" "${FILE_NAME}" "/bloom_data/bloom_data.${1}"
+
+MD5=$(md5sum "/bloom_data/bloom_data.${1}")
+
+echo "${MD5} ${FILE_SIZE}" > "/bloom_data/bloom_data.${1}.meta"
